c8bb39ff3f6a4ff5d986305738e2153126eefb43
Sachin-chamoli <79299103+Sachin-chamoli@users.noreply.github.com>
2022-08-17 00:41:18 +0530

    added search query

--- src/App.js
+++ src/App.js
@@ -4,24 +4,42 @@ import Image from "./component/image"
 import axios from "axios";
 import { useEffect, useState } from 'react';
 function App() {
-  const [state, setState] = useState([])
-    
-    useEffect(()=>{
-            axios.get("https://api.unsplash.com/photos?client_id=5a7HhRzDE_-OgckBUytK7NtY4KgeEIutRDQgYLKioeE")
-            .then((data)=>{
-                console.log(data.data);
-                if(data){
+  const [state, setState] = useState([]);
+  const [search, setSearch] = useState("");
 
-                  setState(data.data);
-                }
-            })
-    },[])
+  // const [query, setQuery] = useState("");
+
+    console.log(search);
+    // useEffect(()=>{
+    //         axios.get("https://api.unsplash.com/photos?client_id=5a7HhRzDE_-OgckBUytK7NtY4KgeEIutRDQgYLKioeE")
+    //         .then((data)=>{
+    //             if(data){
+    //               console.log(data);
+    //               setState(data.data);
+    //             }
+    //         })
+    // },[state]);
+
+const handleClick = ()=>{
+    axios.get(`https://api.unsplash.com/search/photos?page=1&query=${search}&client_id=5a7HhRzDE_-OgckBUytK7NtY4KgeEIutRDQgYLKioeE`)
+    .then((response)=>{
+      console.log(response.data);
+      setState(response.data.results);
+    })
+}
+   
   return (
     <div className="App">
-   {state.map((img,key) =>(
-      <Image key={img.id} src={img.urls.thumb}/>
-   ))
-   }
+      <div>
+        <input type="text" onChange={(e) => setSearch(e.target.value)}/>
+        <button onClick={handleClick}>Search</button>
+      </div>
+      <div>
+        {state.map((img,key) =>(
+              <Image key={img.id} src={img.urls.thumb}/>
+          ))
+        }
+      </div>
     </div>
   );
 }
